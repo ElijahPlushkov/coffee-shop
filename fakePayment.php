7f8057e2f69138755af69e94aed64c7351346282
Elijah Plushkov <134138197+ElijahPlushkov@users.noreply.github.com>
2025-04-13 12:19:03 +0300

    fakePayment.php

--- fakePayment.php
+++ fakePayment.php
@@ -69,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'plushabeststyle@gmail.com';
-        $mail->Password = 'uhfg eyhj urqx firo';
+        $mail->Password = 'здесь должен быть пароль приложения';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
